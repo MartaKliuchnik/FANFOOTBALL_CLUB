4a94b30c4d06e958bf90945cefc2bc892aad73da
Marta <martasagun95@gmail.com>
2024-09-07 12:45:16 +0200

    refactor: update header to match new samples from Figma

--- src/components/Conditions/index.jsx
+++ src/components/Conditions/index.jsx
@@ -1,46 +1,58 @@
-import ActiveButton from "../UI/ActiveButton";
-import styles from "./index.module.css";
+import DeadlineTimer from '../DeadlineTimer';
+import ActiveButton from '../UI/ActiveButton';
+import styles from './index.module.css';
 
-const activeButtonTitle = [
-  {
-    id: 1,
-    title: "Создать команду",
-    width: "253px",
-  },
-  {
-    id: 2,
-    title: "Призы",
-    width: "147px",
-  },
-  {
-    id: 3,
-    title: "Правила",
-    width: "147px",
-  },
-
-  {
-    id: 4,
-    title: "Отзывы",
-    width: "147px",
-  },
+const buttonData = [
+	{
+		id: 1,
+		title: 'Создать команду',
+		width: '253px',
+		className: styles.createTeam,
+	},
+	{
+		id: 2,
+		title: 'Призы',
+		width: '147px',
+		className: styles.mainButton,
+	},
+	{
+		id: 3,
+		title: 'Правила',
+		width: '147px',
+		className: styles.mainButton,
+	},
+	{
+		id: 4,
+		title: 'Отзывы',
+		width: '147px',
+		className: styles.mainButton,
+	},
 ];
 
 export default function Conditions() {
-  return (
-    <section className={styles.conditions}>
-      <div className={styles.container}>
-        <div className={styles.conditionsWrapper}>
-          {activeButtonTitle.map(({ title, id, width, backgroundColor }) => (
-            <ActiveButton
-              className={id == 1 ? styles.buttonLong : null}
-              style={{ width, backgroundColor }}
-              key={id}
-            >
-              {title}
-            </ActiveButton>
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+	return (
+		<section className={styles.conditions}>
+			<div className={styles.container}>
+				<div className={styles.conditionsWrapper}>
+					<div className={styles.leftGroup}>
+						{buttonData.slice(0, 1).map(({ title, id, width, className }) => (
+							<ActiveButton key={id} className={className} style={{ width }}>
+								{title}
+							</ActiveButton>
+						))}
+					</div>
+					<div className={styles.centerGroup}>
+						{buttonData.slice(1).map(({ title, id, width, className }) => (
+							<ActiveButton key={id} className={className} style={{ width }}>
+								{title}
+							</ActiveButton>
+						))}
+					</div>
+					<div className={styles.rightGroup}>
+						<DeadlineTimer initialTime='3:45:58' />
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 }
